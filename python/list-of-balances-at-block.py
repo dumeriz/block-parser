33099330e27ac9e67dd96d2f50dd17a118afaf1f
dumeriz <zdumeril@gmail.com>
2021-10-23 22:19:43 +0200

    Fix: UTXOs that weren't used until the target block weren't removed

--- python/list-of-balances-at-block.py
+++ python/list-of-balances-at-block.py
@@ -15,20 +15,32 @@ def get_blocks_referencing_utxo(utxo):
 def get_balance_change_for(utxo, height):
     return int(red.get(f"znn:change:{utxo}:{height}")) # all numbers are in given in zats (no decimals)
 
+# Returns the sum of balance changes for a given utxo until a given block.
+# Returns -1 if the utxo had no transactions until that block.
 def get_balance_for_utxo_at_block(utxo, block):
     heights = sorted(int(height) for height in get_blocks_referencing_utxo(utxo))
+    if heights[0] > block:
+        return -1
     changes = [get_balance_change_for(utxo, height) for height in heights if height <= block]
     return sum(changes)
 
 def gen_snapshot(blockheight, outputfile):
     all_utxos = get_all_utxos()
+    print(f"Looking at {len(all_utxos)} candidate addresses...")
+
     utxos_sorted = sorted([utxo.decode("utf-8") for utxo in all_utxos])
-    print(f"Producing snapshot for {len(utxos_sorted)} utxos")
     balances = [get_balance_for_utxo_at_block(utxo, blockheight) for utxo in utxos_sorted]
 
+    # associate utxos with balances while removing those that did not have a transaction
+    # until blockheight
+    utxo_balance_pairs = [(utxo, balance) for utxo, balance in zip(utxos_sorted, balances)
+                          if balance > -1]
+
+    print(f"Producing snapshot for {len(utxo_balance_pairs)} relevant addresses.")
+
     with open(outputfile, "w") as f:
-        for line in zip(utxos_sorted, balances):
-            f.write(f"{line[0]}:{line[1]}\n")
+        for pair in utxo_balance_pairs:
+            f.write(f"{pair[0]}:{pair[1]}\n")
 
 if __name__ == "__main__":
     try:
